c8cdfe880337b41e747874cfcae2a8d75a4e9278
Joseph Ryan <jfryan@Josephs-MacBook-Air-2.local>
2023-02-13 13:26:06 -0500

    updated

--- 02-SCRIPTS/build_tpm_matrix.pl
+++ 02-SCRIPTS/build_tpm_matrix.pl
@@ -4,8 +4,9 @@ use strict;
 use warnings;
 use Data::Dumper;
 
-our @IDS = qw(ML25993a ML25998a ML32831a ML218922a ML07312a ML223536a ML078817a ML47742a ML036514a ML129317a);
-our %INX = ('ML25993a'  => 'INXA',
+our %INX = ('ML25997a'  => 'INXB',
+            'ML25999a'  => 'INXD',
+            'ML25993a'  => 'INXA',
             'ML25998a'  => 'INXC',
             'ML32831a'  => 'INXG.1',
             'ML218922a' => 'INXH',
@@ -16,16 +17,12 @@ our %INX = ('ML25993a'  => 'INXA',
             'ML036514a' => 'INXO',
             'ML129317a' => 'INXJ');
 
-our $DIR = '../03-DATA/01-RSEM';
+our $DIR = 'build_tpm_matrix.dir';
 
 our @FILES = qw(B2.genes.results B3a.genes.results B3b.genes.results C2.genes.results C3.genes.results C6.genes.results S1.genes.results S2.genes.results S3.genes.results);
 
-#our $TISSUE = 'tb';
-#our $TISSUE = 'cr';
-#our $TISSUE = 'st';
-
 MAIN: {
-    my $tissue = $ARGV[0] or die "usage: $0 TISSUE (tb, cr, or st)\n";
+    my $tissue = $ARGV[0] or die "usage: $0 TISSUE (tb, cr, or ao)\n";
     my $rh_d = get_data(\@FILES,$DIR,\%INX);
     my $rh_a = adjust_matrix($rh_d,\%INX);
     my $csv  = '';
@@ -51,7 +48,7 @@ sub adjust_matrix {
     foreach my $id (keys %{$rh_d}) {
         $mat{$rh_inx->{$id}}->{'tb'} = [$rh_d->{$id}->{'B2.genes.results'},$rh_d->{$id}->{'B3a.genes.results'},$rh_d->{$id}->{'B3b.genes.results'}];
         $mat{$rh_inx->{$id}}->{'cr'} = [$rh_d->{$id}->{'C2.genes.results'},$rh_d->{$id}->{'C3.genes.results'},$rh_d->{$id}->{'C6.genes.results'}];
-        $mat{$rh_inx->{$id}}->{'st'} = [$rh_d->{$id}->{'S1.genes.results'},$rh_d->{$id}->{'S2.genes.results'},$rh_d->{$id}->{'S3.genes.results'}];
+        $mat{$rh_inx->{$id}}->{'ao'} = [$rh_d->{$id}->{'S1.genes.results'},$rh_d->{$id}->{'S2.genes.results'},$rh_d->{$id}->{'S3.genes.results'}];
     }
     return \%mat;
 }
